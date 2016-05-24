dedf4832dc5e0b2fb1fd9e0605ead23e013c6df8
tobeinword1987 <tobeinword1987@gmail.com>
2016-05-24 20:08:44 +0300

    adding files

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lyudmila
+ * Date: 24.05.16
+ * Time: 19:20
+ */
+//require_once ('namespace/test.php');
+require __DIR__.'/vendor/autoload.php';
+$ex=new test();
+echo $ex->test();
---
+++ namespace/test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lyudmila
+ * Date: 24.05.16
+ * Time: 19:32
+ */
+class test{
+    public function test(){
+        return 1;
+    }
+}
